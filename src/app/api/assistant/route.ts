507d8ad0c23a1fdfb46a154875afe4d750c7a2fb
Egor Ptashnik <jahorup@gmail.com>
2024-11-09 23:18:43 +0100

    feat: init commit

---
+++ src/app/api/assistant/route.ts
@@ -0,0 +1,46 @@
+import OpenAI from 'openai';
+import { AssistantResponse } from 'ai';
+import { TextContentBlock } from 'openai/resources/beta/threads/messages.mjs';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
+
+export async function POST(req: Request) {
+  const input: {
+    threadId: string | null;
+    message: string;
+  } = await req.json();
+
+  const threadId = input.threadId ?? (await openai.beta.threads.create({})).id;
+
+  const createdMessage = await openai.beta.threads.messages.create(threadId, {
+    role: 'user',
+    content: input.message,
+  });
+
+  return AssistantResponse(
+    { threadId, messageId: createdMessage.id },
+    async ({ forwardStream, sendMessage }) => {
+      const run = await openai.beta.threads.runs.createAndPoll(threadId, {
+        assistant_id:
+          process.env.ASSISTANT_ID ??
+          (() => {
+            throw new Error('ASSISTANT_ID environment is not set');
+          })(),
+      });
+
+      if (run.status === "completed") {
+        const messages = await openai.beta.threads.messages.list(threadId, { run_id: run.id });
+        const lastMessage =  messages.data[0];
+        sendMessage({
+          id: lastMessage.id,
+          role: 'assistant',
+          content: lastMessage.content as TextContentBlock[],
+        })
+      } else {
+        console.log("Error");
+      }
+    },
+  );
+}
